fad1ed3f9b4e4e7977d00a21b54e04af541d2b14
ravindu0823 <guestpc87@gmail.com>
2023-10-02 13:22:19 +0530

    feat: created music file pngs

--- new.py
+++ new.py
@@ -1,5 +1,37 @@
 from music21 import *
 
-s = corpus.parse('bach/bwv65.2.xml')
-s.analyze('key')
-s.show()
+environment.set(
+    "musicxmlPath", "C:\\Program Files\\MuseScore 4\\bin\\MuseScore4.exe")
+environment.set("musescoreDirectPNGPath",
+                "C:\\Program Files\\MuseScore 4\\bin\\MuseScore4.exe")
+""" 
+# configure.run()
+s = stream.Stream()
+s.append(meter.TimeSignature('4/4'))
+s.append(note.Rest(type='whole'))
+s.append(note.Note('C4', type='quarter'))
+# s.show()
+# Save as an image file
+s.write('musicxml.png', fp='images/musicxml.png')
+
+
+# score = converter.parse('your_notation_file.xml')
+# stream_to_save = stream.Stream()
+# stream_to_save.append(score)
+
+# stream_to_save.show('musicxml.png')
+ """
+
+# a_note = note.Note("A")
+# c_note = note.Note("C")
+# e_note = note.Note("E")
+
+# # Create a chord object for the A minor chord
+# a_minor_chord = chord.Chord([a_note, c_note, e_note])
+
+# # Show the chord (this will display it in a musical notation)
+# a_minor_chord.show()
+
+b = corpus.parse('bwv66.6')
+bChords = b.chordify()
+bChords.show()
