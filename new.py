7b3297bf190e842751f069f8e3f2644718f3ad5b
ravindu0823 <guestpc87@gmail.com>
2023-10-07 21:41:15 +0530

    refactor: remove unwanted code lines

--- new.py
+++ new.py
@@ -1,37 +1,56 @@
 from music21 import *
+from utils import (
+    CHORD_DATA
+)
+import autochord
 
 environment.set(
-    "musicxmlPath", "C:\\Program Files\\MuseScore 4\\bin\\MuseScore4.exe")
+    "musicxmlPath", "/home/dhanu/.local/bin/MuseScore-4.1.1.232071203-x86_64.AppImage")
 environment.set("musescoreDirectPNGPath",
-                "C:\\Program Files\\MuseScore 4\\bin\\MuseScore4.exe")
+                "/home/dhanu/.local/bin/MuseScore-4.1.1.232071203-x86_64.AppImage")
 """ 
 # configure.run()
 s = stream.Stream()
 s.append(meter.TimeSignature('4/4'))
 s.append(note.Rest(type='whole'))
 s.append(note.Note('C4', type='quarter'))
+s.append(note.Note('D4', type='quarter'))
+s.append(note.Note('E4', type='quarter'))
+s.append(note.Note('F4', type='quarter'))
+s.append(note.Note('G4', type='quarter'))
+s.append(note.Note('A4', type='quarter'))
+s.append(note.Note('B4', type='quarter'))
+s.append(note.Note('C5', type='quarter'))
+s.append(note.Note('D5', type='quarter'))
+s.append(note.Note('E5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
+s.append(note.Note('F5', type='quarter'))
 # s.show()
 # Save as an image file
 s.write('musicxml.png', fp='images/musicxml.png')
-
+ """
 
 # score = converter.parse('your_notation_file.xml')
 # stream_to_save = stream.Stream()
 # stream_to_save.append(score)
 
 # stream_to_save.show('musicxml.png')
- """
 
-# a_note = note.Note("A")
-# c_note = note.Note("C")
-# e_note = note.Note("E")
 
-# # Create a chord object for the A minor chord
-# a_minor_chord = chord.Chord([a_note, c_note, e_note])
+# print(chord.Chord(CHORD_DATA["A:min"], duration=duration.Duration(2.32)))
 
-# # Show the chord (this will display it in a musical notation)
-# a_minor_chord.show()
 
-b = corpus.parse('bwv66.6')
+""" b = corpus.parse('bwv66.6')
 bChords = b.chordify()
-bChords.show()
+bChords.show() """
+
+
+autochord.recognize("audio1.mp3", "audio1.lab")
