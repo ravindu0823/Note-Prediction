22483fd595b7ca46aac96da676500710bb2af5d8
ravindu0823 <guestpc87@gmail.com>
2024-02-01 23:50:00 +0530

    Refactor main.py and remove main2.py

--- main.py
+++ main.py
@@ -1,45 +1,8 @@
-from music21 import *
-from utils import (
-    CHORD_DATA
-)
+import autochord as chord
+# from spleeter.separator import Separator
 
-environment.set(
-    "musicxmlPath", "/home/dhanu/.local/bin/MuseScore-4.1.1.232071203-x86_64.AppImage")
-environment.set("musescoreDirectPNGPath",
-                "/home/dhanu/.local/bin/MuseScore-4.1.1.232071203-x86_64.AppImage")
+# separator = Separator('spleeter:2stems')
+# separator.separate_to_file('audio/final.wav', 'audio/spleeter/')
 
-# Lead lab file
-file_path = 'final.lab'
-
-# Initialize an empty list to store the data
-data = []
-
-# Open the file and read it line by line
-with open(file_path, 'r') as file:
-    for line in file:
-        # Split the line by tab to separate the columns
-        columns = line.strip().split('\t')
-
-        # Convert start and end times to float and store them as a tuple with the label
-        if len(columns) == 3:
-            start_time = float(columns[0])
-            end_time = float(columns[1])
-            label = columns[2]
-            data.append((start_time, end_time, label))
-
-
-
-s = stream.Score()
-s.append(meter.TimeSignature('4/4'))
-
-for start_time, end_time, label in data:
-    time = (end_time - start_time).__round__(2)
-    # print(chord.Chord(CHORD_DATA[label], duration=duration.Duration(time)))
-    new_chord = chord.Chord(CHORD_DATA[label], duration=duration.Duration(time))
-
-    new_measure = stream.Measure()
-    new_measure.append(new_chord)
-    s.append(new_measure)
-
-s.show()
-# s.write('musicxml.png', "images/musicxml.png")
+# print()
+print(chord.recognize("audio/A-Thousand-Years-Christina-Perri.mp3", lab_fn="lab-files/A-Thousand-Years-Christina-Perri-chords.lab"))
--- main2.py
+++
@@ -1,3 +0,0 @@
-import autochord as chord
-
-print(chord.recognize("audio/audio1.mp3"))
